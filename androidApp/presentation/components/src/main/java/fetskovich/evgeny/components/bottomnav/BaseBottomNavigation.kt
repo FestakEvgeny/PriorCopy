cf3bbbf0218c862639ffb342b919f1110dad68d9
Evgeny Fetskovich <e.fetskovich@itransition.com>
2023-09-14 17:10:38 +0300

    bottom navigation item paddings unbounded.

--- androidApp/presentation/components/src/main/java/fetskovich/evgeny/components/bottomnav/BaseBottomNavigation.kt
+++ androidApp/presentation/components/src/main/java/fetskovich/evgeny/components/bottomnav/BaseBottomNavigation.kt
@@ -1,13 +1,18 @@
 package fetskovich.evgeny.components.bottomnav
 
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fetskovich.evgeny.presentation.theme.ApplicationTheme
 import fetskovich.evgeny.presentation.theme.BasicTheme
 
@@ -33,9 +38,22 @@ fun BaseBottomNavigation(
                     }
                 },
                 label = {
-                    Text(
-                        text = tab.title
-                    )
+                    /**
+                     * Because of [BottomNavigationItemHorizontalPadding] (12.dp), we need to
+                     * think (and draw) outside the box.
+                     */
+                    BoxWithConstraints {
+                        Text(
+                            modifier = Modifier
+                                .wrapContentWidth(unbounded = true)
+                                .requiredWidth(maxWidth + 24.dp), // 24.dp = the padding * 2
+                            text = tab.title,
+                            softWrap = false,
+                            textAlign = TextAlign.Center,
+                            maxLines = 1,
+                        )
+                    }
+
                 },
                 alwaysShowLabel = true,
                 selectedContentColor = ApplicationTheme.colors.primaryVariant,
